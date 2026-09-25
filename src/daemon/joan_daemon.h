ec7c0e4997b4ca150d365eaeb15dffe78347f7b4
Adriel Santos <adriel@adriel.eu>
2026-09-24 23:40:22 -0300

    Advance signed sequence for final PTZ safety release

--- src/daemon/joan_daemon.h
+++ src/daemon/joan_daemon.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include <sys/types.h>
 
-#define JOAN_VERSION "0.2.7"
+#define JOAN_VERSION "0.2.8"
 #define JOAN_MAX_BODY (16u * 1024u)
 #define JOAN_MAX_UPDATE_BODY 2097344u
 #define JOAN_MAX_HEADERS 16384u
